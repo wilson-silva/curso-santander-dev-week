9a30ca59c5bb947b2e69284d09ea802edf69476e
wilson <wilson-gs@hotmail.com>
2024-05-23 16:09:22 -0300

    Initial commit

---
+++ src/main/java/dio/me/santander_dev_week/SantanderDevWeekApplication.java
@@ -0,0 +1,13 @@
+package dio.me.santander_dev_week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/me/santander_dev_week/SantanderDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.me.santander_dev_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
